3c1e7e26b04a75fe347ef261d769584cefd9a9a7
Jon Friend <JRF.VRM@gmail.com>
2025-03-28 00:06:01 -0500

    tried favicon for HP; loginOut buttons moved to header; user profile image being displayed.

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,4 +1,5 @@
-import { EllipsisVertical, ShoppingCart , UserIcon} from 'lucide-react';
+import { EllipsisVertical} from 'lucide-react';
+// import { ShoppingCart , UserIcon} from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Sheet,
@@ -7,29 +8,57 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
-import Link from 'next/link';
 import ModeToggle from './mode-toggler';
-// import UserButton from './user-button'; // no idea where exactly this comesfrom/camefrom
+import { auth0 } from "@/lib/auth0"; // from Auth0
+import Image from 'next/image';
+import profileDefault from '@/public/images/HP_logo_02_1024x1024_transp.svg';
 
-const Menu = () => {
+// const Menu = () => {
+export default async function  Menu  ()  {
+
+  const session = await auth0.getSession();
+  const profileImage = session?.user?.picture;
+  
+
   return (
     <div className='flex justify-end gap-3'>
       <nav className='hidden md:flex w-full max-w-xs gap-1'>
+        
         <ModeToggle />
-        {/* <UserButton /> */}
-        <Button asChild variant='ghost'>
+        
+        {/* <Button asChild variant='ghost'>
           <Link href='/cart'>
             <ShoppingCart /> Cart
           </Link>
-        </Button>
+        </Button> */}
 
-        <Button asChild>
-          <Link href='/sign-in'>
-            <UserIcon/>Sign In
-            </Link>
-        </Button>
+        {session ? 
+          <>
+            <a href="/auth/logout">
+              <Button>Logout</Button>
+              </a>
+
+            <Image
+              className='h-8 w-8 rounded-full'
+              src={profileImage || profileDefault}
+              alt=''
+              width={40}
+              height={40}
+              />
+          </>
+        : 
+        <a href="/auth/login">
+            <Button>Sign in / Sign Up</Button>
+            </a>
+      // <Button asChild>
+      //       <Link href='/sign-in'>
+      //         <UserIcon/>Sign In
+      //         </Link>
+      //     </Button>
+        }
 
       </nav>
+
       <nav className='md:hidden'>
         <Sheet>
           <SheetTrigger className='align-middle'>
@@ -37,18 +66,42 @@ const Menu = () => {
           </SheetTrigger>
           <SheetContent className='flex flex-col items-start'>
             <SheetTitle>Menu</SheetTitle>
+            
             <ModeToggle />
-            <Button asChild variant='ghost'>
+            
+            {/* <Button asChild variant='ghost'>
               <Link href='/cart'>
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            {/* <UserButton /> */}
+            
             <Button asChild>
               <Link href='/sign-in'>
                 <UserIcon/>Sign In
               </Link>
-            </Button>
+            </Button> */}
+
+        {session ? 
+          <>
+            <a href="/auth/logout">
+              <Button>Logout</Button>
+              </a>
+
+            <Image
+              className='h-8 w-8 rounded-full'
+              src={profileImage || profileDefault}
+              alt=''
+              width={40}
+              height={40}
+              />
+          </>
+        : 
+          <a href="/auth/login">
+            <Button>Sign in</Button>
+            </a>
+
+        }
+            
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
@@ -57,4 +110,4 @@ const Menu = () => {
   );
 };
 
-export default Menu;
+// export default Menu;
