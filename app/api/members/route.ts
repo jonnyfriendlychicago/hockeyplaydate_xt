4952e03cdf20799b4b35541fbbb9707ed63342c8
Jon Friend <JRF.VRM@gmail.com>
2025-04-10 21:37:27 -0500

    resolved display of outdated results with findMany pages-routes

--- app/api/members/route.ts
+++ app/api/members/route.ts
@@ -1,5 +1,6 @@
 // app/api/members/route.ts
 // this function called by: app/(root)/members/page.tsx
+export const dynamic = 'force-dynamic'; // force fresh fetch on every request; this (along with updated return below) ensures not displaying outdated data
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
@@ -11,5 +12,14 @@ export async function GET() {
     },
   });
 
-  return NextResponse.json(profiles);
+  return NextResponse.json(profiles
+    // begin: this (along with export above) ensures not displaying outdated data
+    , {
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    }
+    // begin: this (along with export above) ensures not displaying outdated data
+
+  );
 }
