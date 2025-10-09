2d7ea32ed8cec04a9f0379a0e24ec60f037a29c7
Jon Friend <JRF.VRM@gmail.com>
2025-10-09 12:02:20 -0500

    howItWorks: finally got this placeholder page done.  Edited file name and contents of the 'groups' folder/page files.

---
+++ app/(root)/how-it-works/page.tsx
@@ -0,0 +1,13 @@
+// app/(root)/how-it-works/page.tsx
+
+export default async function HowItWorks() {
+
+return ( 
+    <main>
+      <h1>How It Works (Placeholder)</h1>
+      <p>
+        This is a placeholder page for what eventually will be the how-to page. <br/>
+      </p>
+    </main>
+  );
+} 
